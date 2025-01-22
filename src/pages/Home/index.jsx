995dfda8047d3a8b4a2a1070eed79787abb842ea
PierreTDX <pierre.tondeux@gmail.com>
2025-01-22 12:51:19 +0100

    Logo Home Page = Gif rejoué une fois

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,30 @@
-import './home.scss'
-import { Link } from 'react-router'
-import Logo from '../../assets/img/logo.svg'
+import { useEffect, useState } from 'react';
+import './home.scss';
+import { Link } from 'react-router';
+import Logo from '../../assets/img/logox1.gif';
 
 function Home() {
+    const [key, setKey] = useState(Date.now()); // État pour gérer le rechargement du GIF
+
+    useEffect(() => {
+        // Met à jour la clé à chaque montage de la page
+        setKey(Date.now());
+    }, []); // Le tableau de dépendances vide signifie que cela s'exécute uniquement au montage
 
     return (
         <>
-            <main className='home'>
-                <img className="homeLogo" src={Logo} alt="Logo SportSee" />
-                <Link to={`/user/12`} className='userLink'>User 12</Link>
-                <Link to={`/user/18`} className='userLink'>User 18</Link>
+            <main className="home">
+                {/* Ajout d'un paramètre unique au src du GIF */}
+                <img
+                    className="homeLogo"
+                    src={`${Logo}?${key}`}
+                    alt="Logo SportSee"
+                />
+                <Link to={`/user/12`} className="userLink">User 12</Link>
+                <Link to={`/user/18`} className="userLink">User 18</Link>
             </main>
         </>
-    )
+    );
 }
 
-export default Home
+export default Home;
